3f8145d5ae42bf72c7add54307c0c2c71cdce53c
1CORE <1core@protonmail.com>
2025-01-27 13:47:05 +0200

    Add handlers middleware

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -13,22 +13,27 @@ import (
 type App struct {
 	*httptreemux.ContextMux
 	shutdown chan os.Signal
+	mw       []Middleware
 }
 
 // Hadler is a type that handles a http request within own "mini framework".
 type Hadler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
 // NewApp created an App value that handle as et og routes for the application.
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
 	return &App{
 		ContextMux: httptreemux.NewContextMux(),
 		shutdown:   shutdown,
+		mw:         mw,
 	}
 }
 
 // Handle sets a handler function foe a given HTTP method and path pair
 // to the application server mux.
-func (a *App) Handle(method string, path string, handler Hadler) {
+func (a *App) Handle(method string, path string, handler Hadler, mw ...Middleware) {
+	handler = wrapMiddleware(mw, handler)
+	handler = wrapMiddleware(a.mw, handler)
+
 	h := func(w http.ResponseWriter, r *http.Request) {
 		// TODO. Additional logic here
 		if err := handler(r.Context(), w, r); err != nil {
