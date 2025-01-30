df012958802fcae60798eb5991a46665e9659903
1CORE <1core@protonmail.com>
2025-01-30 12:35:10 +0200

    Add metrics middleware

--- Makefile
+++ Makefile
@@ -33,6 +33,9 @@ run-help:
 curl:
 	curl -il http://localhost:3000/hack
 
+load:
+	hey -m GET -c 100 -n 100000 "http://localhost:3000/hack"
+
 # ==============================================================================
 # Running from within k8s/kind
 
--- business/web/v1/v1.go
+++ business/web/v1/v1.go
@@ -24,8 +24,10 @@ type RouteAdder interface {
 // APIMux constructs a http.Handler will all application routes defined.
 func APIMux(cfg APIMuxConfig, routeAdder RouteAdder) *web.App {
 	app := web.NewApp(
-		cfg.Shutdown, middlewares.Logger(cfg.Log),
+		cfg.Shutdown,
+		middlewares.Logger(cfg.Log),
 		middlewares.Errors(cfg.Log),
+		middlewares.Metrics(),
 		middlewares.Panics(),
 	)
 
