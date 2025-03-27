2d5f6d1e06e76693cae7fd9500ea7474c3c6a157
1CORE <1core@protonmail.com>
2025-03-27 12:03:56 +0200

    Add auth to user group handler

--- app/services/sales-api/v1/handlers/usergroup/route.go
+++ app/services/sales-api/v1/handlers/usergroup/route.go
@@ -6,6 +6,7 @@ import (
 	"github.com/1core-dev/go-service/business/core/user"
 	"github.com/1core-dev/go-service/business/core/user/stores/userdb"
 	"github.com/1core-dev/go-service/business/web/v1/auth"
+	"github.com/1core-dev/go-service/business/web/v1/middlewares"
 	"github.com/1core-dev/go-service/foundation/logger"
 	"github.com/1core-dev/go-service/foundation/web"
 
@@ -24,8 +25,12 @@ type Config struct {
 func Routes(app *web.App, cfg Config) {
 	const version = "v1"
 
+	authentication := middlewares.Authenticate(cfg.Auth)
+	ruleAdmin := middlewares.Authorize(cfg.Auth, auth.RuleAdminOnly)
+
 	usrCore := user.NewCore(cfg.Log, userdb.NewStore(cfg.Log, cfg.DB))
 
 	handler := New(usrCore, cfg.Auth)
 	app.Handle(http.MethodPost, version, "/users", handler.Create)
+	app.Handle(http.MethodPost, version, "/usersauth", handler.Create, authentication, ruleAdmin)
 }
